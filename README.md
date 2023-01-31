3b2e2b3a4425ad2510af2cd1faabd848b31609e9
eduvinagre <122939534+eduvinagre@users.noreply.github.com>
2023-01-31 16:45:09 -0300

    added files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Menthalist_Game_JS
-Project made for Alura's Imersion Week
+# Menthalist Game - JS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/eduvinagre/pen/oNMaWVB](https://codepen.io/eduvinagre/pen/oNMaWVB).
+
---
+++ script.js
@@ -0,0 +1,24 @@
+var numeroSecreto = parseInt(Math.random() * 1001 + 1)
+
+var limite = 8
+
+var usuario = prompt('Bem vindo ao jogo Mentalista!\nDigite o seu nome para começar')
+    
+while (chute != numeroSecreto) {
+    var chute = prompt (usuario + ', digite um número entre 1 e 1000. Você tem ' + limite + ' tentativas! Digite PARE para desistir')
+    if (chute == numeroSecreto) {
+     alert('Parabéns ' + usuario + '! ' + numeroSecreto +' era o numero secreto!\nFIM DE JOGO')
+    } else if (chute > numeroSecreto) {
+     alert('Errou... ' + chute + ' é MAIOR que o número secreto!')
+    } else if (chute < numeroSecreto) {
+     alert('Errou... ' + chute + ' é MENOR que o número secreto!')
+    } if (limite != numeroSecreto) {
+     limite -= 1
+    } if (limite == 1) {
+    alert('FIM DE JOGO, suas chances acabaram! O número secreto era ' + numeroSecreto)
+    break
+   } if(chute == "PARE") {
+      alert('FIM DE JOGO. O número era ' + numeroSecreto)
+      break;
+   }
+  }
